44e3d4b363f73617dc4285e988c4c35ed9702d6e
Trevor Larson <trevor.larson@smartsheet.com>
2021-11-24 16:30:38 -0800

    Set title

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,15 +1,20 @@
 import 'bootstrap/scss/bootstrap.scss';
 import '../../styles/App.scss';
+import { Helmet } from 'react-helmet'
 import React from 'react';
 import Landing from '../landing'
 import Welcome from '../welcome';
 import About from '../about';
 import Footer from '../footer'
 
+const TITLE = 'The Larson Wedding'
 class App extends React.Component {
     render() {
         return (
             <div className="App">
+                <Helmet>
+                    <title>{ TITLE }</title>
+                </Helmet>
                 <Landing/>
                 <Welcome/>
                 <About/>
