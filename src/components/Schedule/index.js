64b822a88c6bb6c7c18eb8bfef15ca26a4cae914
Trevor Larson <trevor.larson@smartsheet.com>
2022-08-05 22:05:02 -0700

    update schedule

--- src/components/Schedule/index.js
+++ src/components/Schedule/index.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import { Container, Row } from 'react-bootstrap';
 import "../../styles/Schedule.scss";
+import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+import weddingArch from '../../resources/wedding-arch.png'
+import cocktail from '../../resources/cocktail.png'
+import dinner from '../../resources/romantic-dinner.png'
+import cake from '../../resources/wedding-cake.png'
+import dance from '../../resources/dance.png'
+import party from '../../resources/party.png'
+import bus from '../../resources/bus.png'
 
 class Schedule extends React.Component {
   render() {
@@ -10,36 +19,74 @@ class Schedule extends React.Component {
                 <Container className="schedule-msg">
                     <Row className="hdr-msg">
                         <p>
-                            schedule coming soon
+                            schedule
                         </p>
                     </Row>
-                    <h2>Ceremony to begin at 4pm</h2>
-                    {/* <h2>Full schedule coming soon</h2> */}
-                    {/* <Row className="schedule-spacing"/>
-                    <Row className="schedule-txt">
-                        <p>
-                          Item 1
-                        </p>
-                        <p>
-                          Time
-                        </p>
-                    </Row>
-                    <Row className="schedule-txt">
-                        <p>
-                          Item 2
-                        </p>
-                        <p>
-                          Time
-                        </p>
-                    </Row>
-                    <Row className="schedule-txt">
-                        <p>
-                          Item 3
-                        </p>
-                        <p>
-                          Time
-                        </p>
-                    </Row> */}
+                    <VerticalTimeline>
+                      <VerticalTimelineElement
+                        className="vertical-timeline-element"  
+                        dateClassName="timeline-date"
+                        date="four o'clock"
+                        iconStyle={{ background: '#fff', color: '#fff' }} 
+                        icon={<img src={weddingArch}  alt="wedding arch" className='timeline-icon'/>}
+                      >
+                        <h3 className="vertical-timeline-element-title">Ceremony</h3>
+                      </VerticalTimelineElement>
+                      <VerticalTimelineElement
+                        className="vertical-timeline-element"
+                        dateClassName="timeline-date"
+                        date="four thirty"
+                        iconStyle={{ background: '#fff', color: '#fff' }} 
+                        icon={<img src={cocktail}  alt="cocktail" className='timeline-icon'/>}
+                      >
+                        <h3 className="vertical-timeline-element-title">Cocktail Hour</h3>
+                      </VerticalTimelineElement>
+                      <VerticalTimelineElement
+                        className="vertical-timeline-element" 
+                        dateClassName="timeline-date"
+                        date="five forty five"
+                        iconStyle={{ background: '#fff', color: '#fff' }} 
+                        icon={<img src={dinner}  alt="dinner" className='timeline-icon'/>}
+                      >
+                        <h3 className="vertical-timeline-element-title">Dinner</h3>
+                      </VerticalTimelineElement>
+                      <VerticalTimelineElement
+                        className="vertical-timeline-element" 
+                        dateClassName="timeline-date"
+                        date="seven o'clock"
+                        iconStyle={{ background: '#fff', color: '#fff' }} 
+                        icon={<img src={cake}  alt="cake" className='timeline-icon'/>}
+                      >
+                        <h3 className="vertical-timeline-element-title">Cake Cutting</h3>
+                      </VerticalTimelineElement>
+                      <VerticalTimelineElement
+                        className="vertical-timeline-element" 
+                        dateClassName="timeline-date"
+                        date="seven fifteen"
+                        iconStyle={{ background: '#fff', color: '#fff' }} 
+                        icon={<img src={dance}  alt="dance" className='timeline-icon'/>}
+                      >
+                        <h3 className="vertical-timeline-element-title">First Dance</h3>
+                      </VerticalTimelineElement>
+                      <VerticalTimelineElement
+                        className="vertical-timeline-element" 
+                        dateClassName="timeline-date"
+                        date="seven thirty"
+                        iconStyle={{ background: '#fff', color: '#fff' }} 
+                        icon={<img src={party}  alt="party" className='timeline-icon'/>}
+                      >
+                        <h3 className="vertical-timeline-element-title">Party Time</h3>
+                      </VerticalTimelineElement>
+                      <VerticalTimelineElement
+                        className="vertical-timeline-element" 
+                        dateClassName="timeline-date"
+                        date="ten o'clock"
+                        iconStyle={{ background: '#fff', color: '#fff' }} 
+                        icon={<img src={bus}  alt="bus" className='timeline-icon'/>}
+                      >
+                        <h3 className="vertical-timeline-element-title">Guest Transportation</h3>
+                      </VerticalTimelineElement>
+                    </VerticalTimeline>
                 </Container>
             </Container>
       </section>
